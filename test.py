736689206676d5bf718995287b0955f9ef5497e7
Z <70872518+Z0721Z@users.noreply.github.com>
2024-09-19 14:52:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,17 +2,13 @@ import torch
 from torchvision import transforms
 from PIL import Image
 
-# 1. 读取图像
-image_path = "ISBI/images/ISIC_0000007.png" # 替换为实际的图像路径
+image_path = "ISBI/images/ISIC_0000007.png" 
 image = Image.open(image_path)
 
 # 2. 定义图像转换操作
 transform = transforms.Compose([
-    transforms.ToTensor(), # 将图像转换为张量
-
+    transforms.ToTensor(),
 ])
 
-# 3. 应用图像转换
 tensor_image = transform(image)
-# 打印张量的形状
 print("Tensor shape:", tensor_image.shape)
